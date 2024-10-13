4b769d1d23e53d08ef394cee2c354557bd69c81c
FabioLeitao <fabio.tleitao@gmail.com>
2024-10-12 21:10:49 -0300

    App consegue ler um input da opcao escolhida, mas ainda nao faz muito com isso

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ print('Sabor Express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
-print('4. Sair restaurante')
+print('4. Sair restaurante\n')
 
+opcao_escolhida = input('Escolha uma opcao: ')
+print('Voce escolheu a opcao', opcao_escolhida)
